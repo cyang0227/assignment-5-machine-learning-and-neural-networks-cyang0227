11b487fc3bc339209e7795164b2a33e34e8442a4
cyang0227 <cyang0227@gmail.com>
2024-04-09 20:35:03 -0400

    update

--- fashionmnist.py
+++ fashionmnist.py
@@ -4,11 +4,13 @@ import torchvision.transforms.v2 as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 '''
 In this file you will write end-to-end code to train a neural network to categorize fashion-mnist data
 '''
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 '''
 PART 1:
@@ -16,19 +18,21 @@ Preprocess the fashion mnist dataset and determine a good batch size for the dat
 Anything that works is accepted.
 '''
 
-transform = transforms.Compose(''' Fill in this function ''')  # Use transforms to convert images to tensors and normalize them
-batch_size = ''' Insert a good batch size number here '''
-
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5,), (0.5,))
+    ])  # Use transforms to convert images to tensors and normalize them
+batch_size = 10
 '''
 PART 2:
 Load the dataset. Make sure to utilize the transform and batch_size you wrote in the last section.
 '''
 
-trainset = torchvision.datasets.FashionMNIST(''' Fill in this function ''')
-trainloader = torch.utils.data.DataLoader(''' Fill in this function ''')
+trainset = torchvision.datasets.FashionMNIST(root='dataset', download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 
-testset = torchvision.datasets.FashionMNIST(''' Fill in this function ''')
-testloader = torch.utils.data.DataLoader(''' Fill in this function ''')
+testset = torchvision.datasets.FashionMNIST(root='dataset', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
 
 '''
 PART 3:
@@ -36,47 +40,64 @@ Design a multi layer perceptron. Since this is a purely Feedforward network, you
 Do not directly import or copy any existing models.
 '''
 
+# Report split sizes
+print('Training set has {} instances'.format(len(trainset)))
+print('Validation set has {} instances'.format(len(testset)))
+
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
+        #refer to the documentation to see how to use the layers: https://medium.com/@aaysbt/fashion-mnist-data-training-using-pytorch-7f6ad71e96f4
+        self.fc1 = nn.Linear(784, 392)
+        self.fc2 = nn.Linear(392, 146)
+        self.fc3 = nn.Linear(146, 73)
+        self.fc4 = nn.Linear(73, 10)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
+        x = x.view(-1, 784)
+        x = self.dropout(F.relu(self.fc1(x)))
+        x = self.dropout(F.relu(self.fc2(x)))
+        x = self.dropout(F.relu(self.fc3(x)))
+        #sigmoid output layer
+        x = torch.sigmoid(self.fc4(x))
         return x
         
-net = Net()
+net = Net().to(device)
 
 '''
 PART 4:
 Choose a good loss function and optimizer
 '''
 
-criterion = ''' Find a good loss function '''
-optimizer = ''' Choose a good optimizer and its hyperparamters '''
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 '''
 PART 5:
 Train your model!
 '''
 
-num_epochs = '''Choose the number of epochs '''
+num_epochs = 100
+'''Choose the number of epochs '''
 
 for epoch in range(num_epochs):  # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
 
         # forward + backward + optimize
         outputs = net(inputs)
-        loss = criterion(outputs, '''Fill in the blank''')
+        loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
         running_loss += loss.item()
 
-    print(f"Training loss: {running_loss}")
+    print(f"Epoch {epoch+1}, Training loss: {running_loss / len(trainloader)}")
 
 print('Finished Training')
 
@@ -86,13 +107,20 @@ PART 6:
 Evalute your model! Accuracy should be greater or equal to 80%
 
 '''
+net.eval()
 
 correct = 0
 total = 0
 # since we're not training, we don't need to calculate the gradients for our outputs
 with torch.no_grad():
     for data in testloader:
-        ''' Fill in this section '''
+        images, labels = data[0].to(device), data[1].to(device)
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
 print('Accuracy: ', correct/total)
 
--- function_optimization.py
+++ function_optimization.py
@@ -3,7 +3,16 @@ from torch import optim
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
+from matplotlib.ticker import AutoLocator, LinearLocator
+from tqdm import tqdm
 
+print(torch.cuda.is_available())
+
+print(torch.cuda.device_count())
+
+print(torch.cuda.current_device())
+
+print(torch.cuda.get_device_name(0))
 
 def rosenbrock(x, y):
     return (1 - x)**2 + 100 * (y - x**2)**2
@@ -23,23 +32,51 @@ def ackley(x, y):
 ###############################################################################################
 
 
-def plot_function(name: str, x, y, z):
-    fig, ax = plt.subplots(subplot_kw={'projection': '3d'})
-    
+def plot_function(function, save_name, start_input, end_input):
+    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+    # Make data.
+    X = torch.linspace(start_input, end_input, 100)
+    Y = torch.linspace(start_input, end_input, 100)
+    X, Y = torch.meshgrid(X, Y, indexing='ij')
+    Z = function(X, Y)
+
     # Plot the surface.
-    surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm, linewidth=0, antialiased=False)
-    
-    # Customize the z axis.
-    ax.set_zlim(-1.01, 1.01)
-    ax.zaxis.set_major_locator(plt.LinearLocator(10))
-    ax.zaxis.set_major_formatter(plt.FormatStrFormatter('%.02f'))
-    
+    surf = ax.plot_surface(X, Y, Z, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+    # # Customize the z axis.
+    # ax.zaxis.set_major_locator(AutoLocator())
+    # # A StrMethodFormatter is used automatically
+    # ax.zaxis.set_major_formatter('{x:.02f}')
+
     # Add a color bar which maps values to colors.
     fig.colorbar(surf, shrink=0.5, aspect=5)
+    
+    # Set the title
+    ax.set_title(save_name)
 
-    # save the plot
-    plt.savefig(name)
+    # Save the figure
+    plt.savefig(save_name + '.png')
+    
+#plot the rosenbrock function
+plot_function(rosenbrock, 'rosenbrock [-30,30]', -30, 30)
+#clear the plot
+plt.clf()
 
+#plot the beale function
+plot_function(beale, 'beale [-30,30]', -30, 30)
+#clear the plot
+plt.clf()
+
+#plot the ackley function
+plot_function(ackley, 'ackley [-30,30]', -30, 30)
+#clear the plot
+plt.clf()
+
+#plot the ackley function in [-3,3]
+plot_function(ackley, 'ackley [-3,3]', -3, 3)
+#clear the plot
+plt.clf()
 
 ###############################################################################################
 # STOCHASTIC GRADIENT DESCENT
@@ -48,22 +85,54 @@ def plot_function(name: str, x, y, z):
 
 # Use Stochastic Gradient Descent in Pytorch to optimize the objective function.
 
-# Saev the values of the objective function over 5000 iterations in a list.
+# Save the values of the objective function over 5000 iterations in a list.
 
 # Print the values of x, y, and the objective function after optimization.
 ###############################################################################################
+    
+def SGD(function, lr=0.0015, n_iters=5000):
+    x = torch.tensor([10.0], dtype=torch.float, requires_grad=True)
+    y = torch.tensor([10.0], dtype=torch.float, requires_grad=True)
+    optimizer = optim.SGD([x, y], lr=lr)
+    values = []
+    
+    #implement the optimization ref: https://pytorch.org/docs/stable/generated/torch.optim.SGD.html#torch.optim.SGD
+    for i in tqdm(range(n_iters)):
+        optimizer.zero_grad()
+        loss = function(x, y)
+        loss.backward()
+        # Clip gradients to prevent explosion
+        torch.nn.utils.clip_grad_value_([x, y], clip_value=10)
+        optimizer.step()
+        values.append(loss.item())
+    return x, y, values
 
-'''
+#optimize the rosenbrock function
+x, y, values_rosenbrock_SGD = SGD(rosenbrock)
 
+#print the values of x, y, and the objective function after optimization
+print('Rosenbrock function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', values_rosenbrock_SGD[-1])
 
-YOUR CODE HERE
+#optimize the beale function
+x, y, values_beale_SGD = SGD(beale)
 
+#print the values of x, y, and the objective function after optimization
+print('Beale function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', values_beale_SGD[-1])
 
-'''
+#optimize the ackley function
+x, y, values_ackley_SGD = SGD(ackley)
 
-
-
-
+#print the values of x, y, and the objective function after optimization
+print('Ackley function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', values_ackley_SGD[-1])
 
 ###############################################################################################
 # Adam Optimizer
@@ -77,13 +146,47 @@ YOUR CODE HERE
 # Print the values of x, y, and the objective function after optimization.
 ###############################################################################################
 
-'''
+def adam_optim(function, lr=0.0015, n_iters=5000):
+    x = torch.tensor([10.0], requires_grad=True)
+    y = torch.tensor([10.0], requires_grad=True)
+    optimizer = optim.Adam([x, y], lr=lr)
+    values = []
+    #implement the optimization ref: https://pytorch.org/docs/stable/generated/torch.optim.Adam.html#torch.optim.Adam
+    for i in tqdm(range(n_iters)):
+        optimizer.zero_grad()
+        loss = function(x, y)
+        loss.backward()
+        torch.nn.utils.clip_grad_value_([x, y], clip_value=10)
+        optimizer.step()
+        values.append(loss.item())
+    return x, y, values
 
+#optimize the rosenbrock function
+x, y, values_rosenbrock_adam = adam_optim(rosenbrock)
 
-YOUR CODE HERE
+#print the values of x, y, and the objective function after optimization
+print('Rosenbrock function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', rosenbrock(x, y).item())
 
+#optimize the beale function
+x, y, values_beale_adam = adam_optim(beale)
 
-'''
+#print the values of x, y, and the objective function after optimization
+print('Beale function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', beale(x, y).item())
+
+#optimize the ackley function
+x, y, values_ackley_adam = adam_optim(ackley)
+
+#print the values of x, y, and the objective function after optimization
+print('Ackley function')
+print('x:', x.item())
+print('y:', y.item())
+print('Objective function:', ackley(x, y).item())
 
 
 ###############################################################################################
@@ -92,10 +195,22 @@ YOUR CODE HERE
 # Plot the previously stored values of the objective function over 5000 iterations for both SGD and Adam in a single plot.
 ###############################################################################################
 
-'''
+#plot the saved values of the objective function over the iterations
+def plot_values(values, title):
+    plt.plot(values[0], label='SGD')
+    plt.plot(values[1], label='Adam')
+    plt.legend()
+    plt.xlabel('Iterations')
+    plt.ylabel('Objective function')
+    plt.title(title)
+    plt.savefig(title + '.png')
+    plt.clf()
+    
+#plot rosenbrock function values
+plot_values([values_rosenbrock_SGD, values_rosenbrock_adam], 'Rosenbrock SGD vs Adam convergence rates')
 
+#plot beale function values
+plot_values([values_beale_SGD, values_beale_adam], 'Beale SGD vs Adam convergence rates')
 
-YOUR CODE HERE
-
-
-'''
+#plot ackley function values
+plot_values([values_ackley_SGD, values_ackley_adam], 'Ackley SGD vs Adam convergence rates')
